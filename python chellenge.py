9b541cc39db91425e736e9f0110897b684470086
nitrotypepro32 <pskreddy@gmail.com>
2025-03-28 20:01:46 -0500

    Add files via upload

---
+++ python chellenge.py
@@ -0,0 +1,9 @@
+print ("Enter a number  (Numarator) :")
+numn = int(input())
+print ("Enter a number  (Denominator) :")
+numd = int(input())
+
+if numn%numd == 0:
+    print ("The number is divisible")
+else:
+    print ("The number is not divisible")
